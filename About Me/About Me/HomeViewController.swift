885d5142874b2320a31873fca8c8383a7cf66396
Jestin Dorius <jestindorius@gmail.com>
2025-02-06 09:09:37 -0700

    about me lab started

---
+++ About Me/About Me/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  About Me
+//
+//  Created by Jestin Dorius on 2/6/25.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
