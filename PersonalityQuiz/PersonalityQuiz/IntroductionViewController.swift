e7580a847f575a63becca401c17fffda5a7f143b
Jestin Dorius <jestindorius@gmail.com>
2025-02-10 08:59:54 -0700

    Personality Quiz done up to part 3

---
+++ PersonalityQuiz/PersonalityQuiz/IntroductionViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PersonalityQuiz
+//
+//  Created by Jestin Dorius on 2/10/25.
+//
+
+import UIKit
+
+class IntroductionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
