82352319b2c1738537bc3fcd81c4d3cd27b361c4
Jestin Dorius <jestindorius@gmail.com>
2025-02-15 20:38:27 -0700

    About family segue struggles

--- Introduce My Family/Introduce My Family/PersonDetailViewController.swift
+++ Introduce My Family/Introduce My Family/PersonDetailViewController.swift
@@ -9,13 +9,31 @@ import UIKit
 
 class PersonDetailViewController: UIViewController {
 
+    @IBOutlet var personImageView: UIImageView!
+    
+    @IBOutlet var ageNumber: UILabel!
+    
+    var selectedImageName: String?
+    //these two are to enable me to put the different photos in the imageView
+    var personImage: UIImage?
+    var ageNum: String?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        personImageView.image = personImage
+        ageNumber.text = ageNum
     }
-    
-
+    override func performSegue(withIdentifier identifier: String, sender: Any?) {
+        if identifier == "dadTapped" {
+            personImageView.image = UIImage.jestin
+            ageNumber.text = "48"
+        }
+    }
+//    override func performSegue(withIdentifier identifier: String, sender: Any?) {
+//        if segue.identifier == "dadTapped"
+//        personImageView.image =
+//    }
     /*
     // MARK: - Navigation
 
--- Introduce My Family/Introduce My Family/ViewController.swift
+++ Introduce My Family/Introduce My Family/ViewController.swift
@@ -9,10 +9,47 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var dadButton: UIButton!
+    
+    @IBOutlet var momButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    }    
-
+      
+        
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let destinationVC = segue.destination as! PersonDetailViewController
+        if let button = sender as? UIButton {
+            switch button.title(for: .normal) {
+            case "Dad":
+                segue.destination.navigationItem.title = "Jestin W. Dorius"
+                destinationVC.ageNum = "48"
+                if segue.identifier == "dadTapped" {
+                    destinationVC.personImage = UIImage(named: "Jestin")
+                }
+            case "Mom":
+                segue.destination.navigationItem.title = "Heather Dorius"
+                destinationVC.ageNumber.text = "48"
+            case "Older Sister":
+                segue.destination.navigationItem.title = "Emmalee Dorius"
+            case "Older Brother":
+                segue.destination.navigationItem.title = "Cody Dorius"
+            case "Me":
+                segue.destination.navigationItem.title = "Jestin C. Dorius"
+            case "Younger Brother":
+                segue.destination.navigationItem.title = "Caleb N. Dorius"
+                
+            default: break
+            }
+        }
+//        if segue.identifier == "dadTapped" {
+//            segue.destination.navigationItem.title = "Jestin"
+//            destinationVC.personImageView = "Jestin"
+//        } else if segue.identifier == "momTapped" {
+//            segue.destination.navigationItem.title = "Heather"
+//        }
+    }
 }
 
