ad1ab86f29acc60ac64c8f086e248dfbda5aca4b
Jestin Dorius <jestindorius@gmail.com>
2025-02-21 12:37:17 -0700

    Text Validation app complete(took me an hour longer than class

---
+++ TextValidation/TextValidation/ViewController.swift
@@ -0,0 +1,60 @@
+//
+//  ViewController.swift
+//  TextValidation
+//
+//  Created by Jestin Dorius on 2/21/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var passwordTextField: UITextField!
+    
+    @IBOutlet var errorMessageLabel: UILabel!
+    @IBOutlet var loginSuccessLabel: UILabel!
+    
+    @IBOutlet var submitButton: UIButton!
+    
+    let specialCharacters = ",.!@#$%^&*<>?+=-"
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        updateUI()
+        loginSuccessLabel.layer.cornerRadius = 10
+        loginSuccessLabel.layer.masksToBounds = true
+        errorMessageLabel.layer.cornerRadius = 10
+        errorMessageLabel.layer.masksToBounds = true
+    }
+    
+    func updateUI() {
+        errorMessageLabel.isHidden = true
+        loginSuccessLabel.isHidden = true
+    }
+    
+    @IBAction func submitButtontapped(_ sender: Any) {
+        guard let passwordTextFieldText = passwordTextField.text else { return }
+        
+        if passwordTextFieldText.count < 8 {
+            errorMessageLabel.isHidden = false
+            errorMessageLabel.text = "ERROR!!: The password length needs to be more than 8 characters long."
+            loginSuccessLabel.isHidden = true
+            return
+        }
+        if !passwordTextFieldText.contains(where: { specialCharacters.contains($0) }) {
+            errorMessageLabel.isHidden = false
+            errorMessageLabel.text = "ERROR!!: The password must contain a special character."
+            loginSuccessLabel.isHidden = true
+            return
+        }
+        
+        loginSuccessLabel.isHidden = false
+        loginSuccessLabel.text = "Awesome, your password contains the correct elements needed for security purposes!"
+        errorMessageLabel.isHidden = true
+    }
+    
+    
+}
+
---
+++ TextValidation/TextValidationTests/TextValidationTests.swift
@@ -0,0 +1,17 @@
+//
+//  TextValidationTests.swift
+//  TextValidationTests
+//
+//  Created by Jestin Dorius on 2/21/25.
+//
+
+import Testing
+@testable import TextValidation
+
+struct TextValidationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
