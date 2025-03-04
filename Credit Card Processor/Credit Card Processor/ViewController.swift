535035f809303f213416e25afea3a8b30058e505
Jestin Dorius <jestindorius@gmail.com>
2025-03-04 10:24:02 -0700

    Credit Card Process app started

---
+++ Credit Card Processor/Credit Card Processor/ViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ViewController.swift
+//  Credit Card Processor
+//
+//  Created by Jestin Dorius on 3/4/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    class CreditCard {
+        var cardNumber: Int
+        var cardHolderName: String
+        var expiration: Int
+        var cvv: Int
+        
+        init(cardNumber: Int, cardHolderName: String, expiration: Int, cvv: Int) {
+            self.cardNumber = cardNumber
+            self.cardHolderName = cardHolderName
+            self.expiration = expiration
+            self.cvv = cvv
+        }
+    }
+    
+    protocol Card {
+        func validCardNumber()
+        func creditCardCheck()
+    }
+    
+    //this checks to be sure that there is a card on file
+    class cardOnFile: Card {
+        func validCardNumber() {
+            print("The card is a valid credit card")
+        }
+        func creditCardCheck() {
+            print("Credit card is on file")
+        }
+    }
+    
+    //This will be used to process the card
+    class creditCardProcess {
+        func processCreditCard() {
+            print("payment was processed")
+        }
+    }
+    
+    
+    
+    
+
+}
+
---
+++ Credit Card Processor/Credit Card ProcessorTests/Credit_Card_ProcessorTests.swift
@@ -0,0 +1,17 @@
+//
+//  Credit_Card_ProcessorTests.swift
+//  Credit Card ProcessorTests
+//
+//  Created by Jestin Dorius on 3/4/25.
+//
+
+import Testing
+@testable import Credit_Card_Processor
+
+struct Credit_Card_ProcessorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
