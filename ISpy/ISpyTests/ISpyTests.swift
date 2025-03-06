991ba93c0aad2dfd89995855643135557158bea5
Jestin Dorius <jestindorius@gmail.com>
2025-03-06 09:49:05 -0700

    ISpy done, but it doesn't zoom for some reason

---
+++ ISpy/ISpyTests/ISpyTests.swift
@@ -0,0 +1,17 @@
+//
+//  ISpyTests.swift
+//  ISpyTests
+//
+//  Created by Jestin Dorius on 3/6/25.
+//
+
+import Testing
+@testable import ISpy
+
+struct ISpyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
