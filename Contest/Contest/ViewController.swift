38429c5b5a6393fa111a4b4fd516031566819735
Jestin Dorius <jestindorius@gmail.com>
2025-04-18 22:35:21 -0600

    Contest Done

--- Contest/Contest/ViewController.swift
+++ Contest/Contest/ViewController.swift
@@ -12,23 +12,39 @@ class ViewController: UIViewController {
     @IBOutlet weak var emailTextfield: UITextField!
     @IBOutlet weak var submitButton: UIButton!
     
+    @IBOutlet weak var emailStack: UIStackView!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        emailStack.layer.cornerRadius = 20
     }
-
+    
     @IBAction func submitButtonTapped(_ sender: Any) {
-        if emailTextfield.state.isEmpty {
-            var buttonBackground: UIView = submitButton
-            UIView.animate(withDuration: 4, animations: {
-                let transform = CGAffineTransform(scaleX: 3.0, y: 2.0)
-                buttonBackground.transform = transform
-            })
+        if emailTextfield.text?.isEmpty ?? true {
+            print("textfield empty?")
+            let buttonBackground: UIView = submitButton
+            let emailView: UIView = emailStack
+            UIView.animate(withDuration: 0.2, animations: {
+                let translate = CGAffineTransform(translationX: 10, y: 0)
+                let scale = CGAffineTransform(scaleX: 2.0, y: 1)
+                buttonBackground.transform = translate
+                emailView.transform = scale
+                emailView.backgroundColor = .red
+            }) { (_) in
+                UIView.animate(withDuration: 0.1) {
+                    buttonBackground.transform = CGAffineTransform.identity
+                    emailView.transform = CGAffineTransform.identity
+                    emailView.backgroundColor = .systemGray
+                }
+            }
+            return
         } else {
+            print("textfield not empty")
             performSegue(withIdentifier: "submitTapped", sender: sender)
         }
+        return
     }
     
 }
