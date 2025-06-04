20cfc33399cc569a0631e9f2fc04e53c65302aae
Jestin Dorius <jestindorius@gmail.com>
2025-06-04 10:02:14 -0600

    Geometry Reader done

--- GeometryReader/GeometryReader/ContentView.swift
+++ GeometryReader/GeometryReader/ContentView.swift
@@ -7,31 +7,49 @@
 
 import SwiftUI
 
+@Observable
+class Size {
+    var rowCount: Int = 0
+    var columnCount: Int = 0
+    
+    
+}
+
 struct ContentView: View {
     @Environment(\.verticalSizeClass) var verticalSizeClass
     @Environment(\.horizontalSizeClass) var horizontalSizeClass
-    @State private var columnCount = 0
+    @State var sizeClass = Size()
+    @State var horiReg = Bool()
+    @State private var rowCount = 0
     var body: some View {
         
         GeometryReader { geometry in
             
-            if horizontalSizeClass == .regular {
+            if horizontalSizeClass == .compact {
                 
             }
+            // Grid for rows and columns
             VStack {
-                Image(systemName: "person.fill")
-                    .resizable()
-                    .frame(width: geometry.size.width / 2, height: geometry.size.height / 4)
-                Text("First Employee")
+                // Rows
+                LazyHGrid(rows: [
+                    GridItem(.fixed(100))]) {
+                        // Individual Item
+                        VStack {
+                            Image(systemName: "person.fill")
+                                .resizable()
+                                .frame(width: geometry.size.width / 2, height: geometry.size.height / 4)
+                            Text("First Employee")
+                        }
+                    }
             }
             .background(Color.gray)
             .cornerRadius(15)
             .padding()
-            VStack {
-                Image(systemName: "person.fill")
-                    .resizable()
-                    .frame(width: geometry.size.width / 2, height: geometry.size.height / 4)
-            }
+//            VStack {
+//                Image(systemName: "person.fill")
+//                    .resizable()
+//                    .frame(width: geometry.size.width / 2, height: geometry.size.height / 4)
+//            }
         }
 //        GeometryReader { screenSize in
 //            VStack {
@@ -59,6 +77,16 @@ struct ContentView: View {
 //            }
 //        }
     }
+}
+@ViewBuilder
+func createEmployeeCard() -> some View {
+    VStack {
+        Image(systemName: "person.fill")
+        Text("Emoployee")
+    }
+    .background(Color.gray)
+    .cornerRadius(15)
+    .padding()
 }
 
 #Preview {
